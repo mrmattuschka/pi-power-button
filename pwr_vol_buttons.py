e62aa1a7708fc9e48983af09d9702ac2627a99ae
mrmattuschka <alex@familie-mattausch.de>
2020-05-04 22:32:16 +0200

    made vol change quiet, added speaker shutdown

--- pwr_vol_buttons.py
+++ pwr_vol_buttons.py
@@ -2,20 +2,25 @@
 
 import time
 import subprocess
-from gpiozero import Button, LED
+import atexit
+from gpiozero import Button, LED, DigitalOutputDevice
 
-##### Button actions #####
+# Button actions
+
+speaker = DigitalOutputDevice(22, active_high=True, initial_value=True)
 
 def vol_up():
-    subprocess.run("amixer set PCM 5%+", shell=True, check=True)
+    subprocess.run("amixer -q set PCM 5%+", shell=True, check=True)
 
 def vol_dn():
-    subprocess.run("amixer set PCM 5%-", shell=True, check=True)
+    subprocess.run("amixer -q set PCM 5%-", shell=True, check=True)
 
 def shutdown():
+    speaker.off()
     subprocess.run("shutdown -h now", shell=True, check=True)
+    
 
-##### Set up buttons #####
+# Set up buttons
 
 button_vol_up   = Button(17, pull_up=True, hold_time=0.1)
 button_vol_up.when_held = vol_up
@@ -25,6 +30,12 @@ button_vol_dn.when_held = vol_dn
 
 button_shutdown = Button(3, pull_up=True, hold_time=0.1)
 button_shutdown.when_held = shutdown
+
+# Make sure to turn off speakers when this script exits
+
+atexit.register(speaker.off)
+
+# Most importantly: keep doing nothing
 
 while True:
     time.sleep(10)
